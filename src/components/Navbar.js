97a15573011e89ef5690ee10b426c51eed0c5090
fahim32446 <azmirhasan.az2@gmail.com>
2022-06-28 23:01:17 +0600

    2nd Update slider fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ padding: 10px 20px;
 display:flex;
 justify-content: space-between;
 align-items: center;
-${mobile({padding: "10px 0px"})};
+${mobile({ padding: "10px 0px" })};
 `
 const Left = styled.div`
 flex: 1;
@@ -25,18 +25,18 @@ align-items: center;
 const Language = styled.span`
 font-size: 14px;
 cursor: pointer;
-${mobile({display: "none"})};
+${mobile({ display: "none" })};
 `
 const SearchContainer = styled.div`
-border: 1px solid black;
+border: 1px solid lightgrey;
 display: flex;
 align-items: center;
 margin-left:25px;
-padding: 5px;
 `
 const Input = styled.input`
 border: none;
-${mobile({width: "50px"})};
+padding: 5px;
+${mobile({ width: "50px" })};
 
 `
 
@@ -46,7 +46,7 @@ flex: 1;
 const Logo = styled.h1`
 font-weight: bold;
 text-align: center;
-${mobile({fontSize: "20px"})};
+${mobile({ fontSize: "20px" })};
 `
 
 const Right = styled.div`
@@ -54,13 +54,13 @@ flex: 1;
 display: flex;
 align-items: center;
 justify-content: flex-end;
-${mobile({justifyContent: "center", flex: "2"})};
+${mobile({ justifyContent: "center", flex: "2" })};
 `
 const MenuItem = styled.div`
 font-size: 14px;
 cursor:pointer;
 margin-left:25px;
-${mobile({fontSize: "12px", marginLeft: "10px"})};
+${mobile({ fontSize: "12px", marginLeft: "10px" })};
 `
 
 const Navbar = () => {
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,76 +4,97 @@ import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { sliderItems } from '../data';
 import { mobile } from "../responsive";
+import Grid from '@mui/material/Grid';
+
 
 const Container = styled.div`
-width: 100%; 
-height: 100vh; 
-display: flex; 
-position: relative;
-overflow: hidden;
-${mobile({ display: "none" })}
+    width: 100%; 
+    display: flex; 
+    position: relative;
+    overflow: hidden;
+    ${mobile({ marginBottom: "10vw" })}
 `
 const Arrow = styled.div`
-width: 30px;
-height: 30px;
-background-color: #fff;
-border-radius:50%;
-display: flex;
-align-items: center;
-justify-content: center;
-position: absolute;
-top: 0px;
-bottom: 100px;
-left:${p => p.direction === "left" && "10px"};
-right:${p => p.direction === "right" && "10px"};;
-margin:auto;
-cursor:pointer;
-z-index:2;
+    width: 30px;
+    height: 30px;
+    background-color: #fff;
+    border-radius:50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    left:${p => p.direction === "left" && "10px"};
+    right:${p => p.direction === "right" && "10px"};;
+    margin:auto;
+    cursor:pointer;
+    z-index:2;
+  
 
 `
 const Wrapper = styled.div`
-height:100%;
-display: flex;
-transform: translateX(${(props) => props.slideIndex * -100}vw);
-transition: all 1s;
+    height:100%;
+    display: flex;
+    transform: translateX(${(props) => props.slideIndex * -100}vw);
+    transition: all 1s;
 `
 const Slide = styled.div`
-display:flex;
-align-items: center;
-width: 100vw;
-height: 100vh;
-
-
+    display:flex;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    ${mobile({ height: "50vh", position: "relative" })}
+    
 `
 const ImgContainer = styled.div`
-flex:1;
-height: 100%;
+    flex:1;
+    height: 100%;
 `
 const Image = styled.img`
     height: 80%;
+    ${mobile({ width: "100vw", height: "50vh", objectFit: "cover" })}
 `
-const InfoContainer = styled.div`
-flex:1;
-padding: 50px;
 
+const InfoContainer = styled.div`
+    flex:1;
+    padding: 50px;
+    ${mobile({ position: "absolute" , display: "flex", flexDirection: "column", justifyContent: "center",
+    alignContent: "center"})};
 `
 const Title = styled.h1`
-font-size: 70px;
-`;
+    font-size: 70px; 
+    ${mobile({ fontSize: "20px", textAlign: "center", background: "rgba(0,0,0,0.5)", color: "white", borderRadius: "10px", padding: "10px" })};
+    
+`
 
 const Desc = styled.p`
-  margin: 50px 0px;
-  font-size: 20px;
-  font-weight: 500;
-  letter-spacing: 3px;
-`;
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+    
+    ${mobile({ fontSize: "15px", textAlign: "center", margin: "20px", background: "rgba(0,0,0,0.5)", color: "white", borderRadius: "10px", padding: "10px"})};
+`
 
 const Button = styled.button`
-  padding: 10px;
-  font-size: 20px;
-  background-color: transparent;
-  cursor: pointer;
-`;
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+    ${mobile({  padding: "8px",  fontSize: "10px", width: "30%", backgroundColor: "white", border: "none",
+    position:"absolute",
+    top: "80%",
+    left: "32vw",
+    
+})}
+&:hover{
+    background-color: teal;
+    color: white;
+    transition: all 0.1s linear;
+}
+ 
+`
 
 const Slider = () => {
 
@@ -94,14 +115,17 @@ const Slider = () => {
             <Wrapper slideIndex={slideIndex}>
                 {sliderItems.map((item) => (
                     <Slide bg={item.bg} key={item.id}>
+
                         <ImgContainer>
                             <Image src={item.img} />
                         </ImgContainer>
+
                         <InfoContainer>
                             <Title>{item.title}</Title>
                             <Desc>{item.desc}</Desc>
                             <Button>SHOW NOW</Button>
                         </InfoContainer>
+
                     </Slide>
                 ))}
 
